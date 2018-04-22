66cef975537f0c693bdb98ab22b812a82bd7bf36
Andreas Rybin <ryan1011@hs-karlsruhe.de>
2018-04-22 21:55:43 +0200

    2. try

---
+++ app/src/main/java/com/example/andreas/test/MyGLRenderer.java
@@ -0,0 +1,25 @@
+package com.example.andreas.test;
+
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.opengles.GL10;
+
+public class MyGLRenderer implements GLSurfaceView.Renderer {
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, javax.microedition.khronos.egl.EGLConfig config) {
+        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 unused) {
+        // Redraw background color
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 unused, int width, int height) {
+        GLES20.glViewport(0, 0, width, height);
+    }
+}
